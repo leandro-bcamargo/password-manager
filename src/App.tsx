f3b9732f30930a9ceba3ba2680317d85391752ea
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-02-12 20:47:13 -0300

    Create form fields validation

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 import Form from './components/Form';
 
 function App() {
-  const [registerBtnVisiblity, setRegisterBtnVisibility] = useState(true)
-  const [formVisibility, setFormVisibility] = useState(false)
+  const [registerBtnVisiblity, setRegisterBtnVisibility] = useState(true);
+  const [formVisibility, setFormVisibility] = useState(false);
 
   function handleRegister() {
     setRegisterBtnVisibility(false);
@@ -16,17 +16,17 @@ function App() {
     setFormVisibility(false);
   }
 
-  function handleClick (e: React.MouseEvent<HTMLButtonElement>) {
-    const {name} = e.currentTarget;
+  function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+    const { name } = e.currentTarget;
     if (name === 'register') handleRegister();
     if (name === 'cancel') handleCancel();
   }
 
   return (
     <>
-    <h1>Gerenciador de senhas</h1>
-    {registerBtnVisiblity && <button name="register" onClick={handleClick}>Cadastrar nova senha</button>}
-    {formVisibility && <Form handleClick={handleClick}/>}
+      <h1>Gerenciador de senhas</h1>
+      {registerBtnVisiblity && <button name="register" onClick={ handleClick }>Cadastrar nova senha</button>}
+      {formVisibility && <Form handleClick={ handleClick } />}
     </>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,80 @@
+import React, { useState } from 'react';
+
 type FormProps = {
   handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 export default function Form({ handleClick }: FormProps) {
+  const [formData, setFormData] = useState({
+    serviceName: '',
+    login: '',
+    password: '',
+    url: '',
+  });
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  function validateServiceName(serviceName: string) {
+    if (serviceName) return true;
+  }
+
+  function validateLogin(login: string) {
+    if (login) return true;
+  }
+
+  function validatePassword(password: string) {
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    }
+    const lettersNumsRegex = /(?=.*[0-9])(?=.*[a-zA-Z])/;
+    if (!lettersNumsRegex.test(password)) {
+      return false;
+    }
+    const specialCharsRegex = /[^0-9a-zA-Z]/;
+    return !(!specialCharsRegex.test(password));
+  }
+
+  function validateForm() {
+    if (validateServiceName(formData.serviceName)
+      && validateLogin(formData.login)
+      && validatePassword(formData.password)) {
+      setIsFormValid(true);
+    }
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.currentTarget;
+    setFormData({ ...formData, [name]: value });
+    validateForm();
+  }
+
   return (
     <form action="submit">
       <label htmlFor="service-name">Nome do serviço</label>
-      <input type="text" id="service-name" />
+      <input
+        value={ formData.serviceName }
+        name="serviceName"
+        type="text"
+        id="service-name"
+        onChange={ handleChange }
+      />
       <label htmlFor="login">Login</label>
-      <input type="text" id="login" />
+      <input
+        onChange={ handleChange }
+        value={ formData.login }
+        name="login"
+        type="text"
+        id="login"
+      />
       <label htmlFor="password">Senha</label>
-      <input type="password" />
+      <input
+        onChange={ handleChange }
+        value={ formData.password }
+        name="password"
+        type="password"
+      />
       <label htmlFor="url">URL</label>
-      <input type="text" />
-      <button type="submit">Cadastrar</button>
+      <input onChange={ handleChange } value={ formData.url } name="url" type="text" />
+      {isFormValid && <button name="register" type="submit">Cadastrar</button>}
       <button name="cancel" onClick={ handleClick }>Cancelar</button>
     </form>
   );
